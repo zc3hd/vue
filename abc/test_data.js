5232d69d5370023c8b5739fc3618b5448352dcc3
armincc <343371169@qq.com>
2018-11-02 17:11:35 +0800

    add plann dir

---
+++ abc/test_data.js
@@ -0,0 +1,293 @@
+'use strict';
+
+
+// 所有的计划
+var plans_data = [
+  // 
+  // ======================================Vue1.0
+  {
+    name: "vue-1",
+    date: '2018-09-12',
+    info: `
+    DMOE:demo 1-4/1-1
+    1、简单的指令
+    2、事件对象$event、阻止冒泡 @click.stop = "_show()" 、:_key = 'key';
+    3、生命周期 ready() 
+       原型方法 VUE.directive()自定义指令就是DOM操作  
+       vm.$mount('#app')  
+       vm.$watch('a',function (argument) {})
+
+
+    4、动画：不同的状态，就是该DOM样式名称的不同的值么。这个样式本是个string，也是就是一个数据
+    5、子组件的初始化，注册组件。Vue.extend();
+    
+    6. 组件选项卡 <component :is='table_name'></component>  table_name变量为组件名称
+       
+
+
+    7. f-->s 初始化s组件，props接受属性字段。在子组件的使用中，作为属性进行绑定f的变量 【随动】
+    8. f<--s 初始化s组件，在z的组件的交互中、或者ready() 中定义 发射事件，在子组件的使用中，相当于
+       子组件 发生该事件，f组件用自己定义的事件进行接受数据。【不会动，只能在再次发射】
+    `,
+  },
+
+  // 
+  {
+    name: "vue-2",
+    date: '2018-09-14',
+    info: `
+    ****************************************************
+    DMOE: demo-5 | webpack/demo-6
+
+    1、slot 感觉没有什么用，就是一个槽位占位
+    2、router初始化过程 
+        HTML：v-link指定name-->path，router-view的窗口展  
+        JS：path-->component  初始化一个组件容器 定义各个路由下的组件 组件和路由进行关联 定义路由的转跳 开启路由。
+    3、个人感觉路由嵌套没有什么用，真实的场景中用到的多层级菜单都是一个层级的路由。
+
+    4.webpack vue-loader .vue
+        npm install 一定的版本号
+        入口文件是main.js body作为根组件。APP.vue注入了，原因是要把所有的东西都写在 .vue的组件里。
+        webpack 编译的也是JS文件。里面有.vue文件
+        
+        需要HTML模本 是我们已经设置好的index.html
+
+
+    ****************************************************
+    DMOE: demo-5 | webpack/demo-7
+
+    1、demo-5 基础版的改版 路由视图组件App 这样的写法
+    2. webpack vue-router的用法 和上面一样。
+      .vue 文件是vue-loader编译的。无需 import Vue from 'vue';
+    `,
+  },
+
+  // 
+  {
+    name: "vue-3",
+    date: '2018-09-18',
+    info: `
+    ****************************************************
+    DMOE: webpack/demo-8 | demo-9
+
+    demo-8:
+    1、vue-cli的webpack-simple 初始化,有export可以导入并被一起打包，
+       没有export就直接 引入index里就可以了。
+       vue init webpack#1.0 .
+    
+
+    demo-9:
+    2、Vue2.0 
+
+       1.template需要根DOM节点
+       2.{...}直接对象形式初始化组件，代替1.0-->Vue.extend({...})
+       
+       3.生命周期 beforeCreate created beforeMount mounted ..
+       
+       4.v-for='(ele,index) in arr'
+
+       5.Vue.filter('filter_name',function (a,b,c) {
+           传入前面的值
+         })
+         {{msg | filter_name(1,2)}}
+
+       6.父子之间的通信：废弃同步sync :msg.sync = 'msg'
+         方案就是父给子传递Object ，传的是地址，改变属性就同步改变。
+
+       7.组件之间的通信 单一事件管理组件通信
+       var ev = new Vue();
+
+       cp_1:
+       send(){
+        var me = this;
+        ev.$emit('send_ev',me.msg);
+       }
+
+       cp_2:
+       mounted(){
+        var me = this;
+        ev.$on('send_ev',function (msg) {
+          me.msg = msg;
+        })
+       }
+    `,
+  },
+
+  // 
+  {
+    name: "vue-4",
+    date: '2018-09-21',
+    info: `
+    ****************************************************
+    DMOE: demo-10 | webpack/demo-11 
+
+    demo-10:
+    1、vue2.0-router的在HTML中使用:
+       视图：
+       <router-link to ='/nav_1'>nav_1</router-link>
+       <router-view></router-view>
+
+       配置：
+       var routes = [
+        {path:"/nav_1",component:"nav_1"},
+       ];
+       var router = new VueRouter({
+        routes:routes
+       });
+
+       开起
+       new vue({
+        el:"#app",
+        router:router
+       })
+       ------------------------------------------------
+       视图也可以放在组件内部，注意根节点
+
+       开启
+       new vue({
+        el:"#app",
+        router:router，
+        render:h=>h(view_cpt);
+       })
+
+    webpack/demo-11：
+    1、vue2.0-router的在webpack中使用:
+      npm install vue-router@2.0.1 --save
+
+      import VueRouter form 'vue-router';
+      vue.use(VueRouter);
+
+      var router = new VueRouter({
+        routes:routes
+      });
+
+      new vue({
+        el:"#app",
+        render:h=>h(App),
+        router,
+      })
+    `,
+  },
+
+
+  // 
+  {
+    name: "vue-5",
+    date: '2018-09-27',
+    info: `
+    ****************************************************
+    DMOE: webpack/demo-12 
+
+    demo-12:
+     1.element-ui 的全局引入和局部引入。
+     2. npm i xxx@0.0.1 -D /-S
+     3. axios ...
+     4. element-ui的组件可以绑定事件，组件写的组件不能绑定，类似一种事件代理：@click.native = 'xxx';
+
+     Z.element-ui 目前在我这没有什么用。
+    `,
+  },
+
+  // 
+  {
+    name: "vue-6",
+    date: '2018-09-29',
+    info: `
+    ****************************************************
+    DMOE: webpack/demo-13 
+
+    demo-13: vuex
+     1.vuex 暂时的数据中心，可以传数据，传方法。
+     2.在index.js里直接写 state 和 mutations就行了。
+     3.在组件内部直接使用 
+        me.$store.state.xxx 进行数据的获取
+        me.$store.commit('ev_1',info||fn) 进行数据或方法的传递传递。
+     4.相比之下，老师教的那个又复杂，又不好维护。
+    `,
+  },
+
+  // 
+  {
+    name: "vue-7",
+    date: '2018-10-26',
+    info: `
+    ****************************************************
+    DMOE: webpack/demo-14 
+
+    demo-14: 项目实战
+     1.router模式、回滚、路径重定向。
+     2.有些组件的状态的控制指导用在什么地方会更好。
+     3.axios的一些配置问题。使用技巧。
+     4.此后的复习都是全部从前到后看一次。
+    `,
+  },
+
+
+  // ======================================En
+  {
+    name: "webpack-1",
+    date: '2018-11-2',
+    info: `
+    
+    demo: demo_webpack_learn
+
+    001：算是对目前开发单个模块（js）的webpack开发打包工具了。
+
+    1.dev模式：入口html 、入口JS、less的配置即可；
+    2.build模式：
+       输出路径、
+
+       输出name、
+       入口js（和异步JS）的输出配置
+       css的输出配置
+
+       html的指定和输出，这个插件可以灵活的处理js和css的路径，但是不能处理css里面的路径
+
+       所以：img、fonts的输出路径是由src里面目录结构决定的，也是决定了css的输出路径
+    `,
+  },
+
+
+
+
+
+
+
+
+
+
+
+
+  // ======================================En
+  {
+    name: "list-5",
+    date: '2018-09-11',
+    info: `
+    `,
+  },
+  // 
+  {
+    name: "list-1",
+    date: '2018-09-12',
+    info: `
+    `,
+  },
+  // 
+  {
+    name: "list-6",
+    date: '2018-09-18',
+    info: `
+    `,
+  },
+
+  // 
+  {
+    name: "en-001",
+    date: '2018-10-25',
+    info: `
+    `,
+  },
+
+
+
+];

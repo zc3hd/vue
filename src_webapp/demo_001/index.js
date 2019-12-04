f17408fe2702640f15b81a278af9c6242e350eb1
zc3hd <23397295+zc3hd@users.noreply.github.com>
2019-12-04 18:51:43 +0800

    demo_001:toDoList

--- src_webapp/demo_001/index.js
+++ src_webapp/demo_001/index.js
@@ -3,323 +3,121 @@ var vm = new Vue({
   el: '#app',
   // 数据
   data: {
-    info: 'w-vue',
-    // ================================================
+    // ----------------------------------------------基础学习
     obj: {
       a: 'admin',
       b: 'bdmin',
       c: 'cdmin',
       d: 'ddmin',
     },
+    arr: [1, 1, 1, 3, 4],
+    // 按钮控制的显示
+    key: false,
 
 
 
-    // ================================================
-    arr: arr,
-    // 用户的对象体
-    user_obj: {
-      // 整体弹窗的现实
-      show: false,
-
-      // 信息体
-      title: '',
-      name: '',
-      // 当前点击的ID
-      ac_id: null,
-
-      // 添加那条的显示
-      show_add: true,
-      // 删除那条隐藏
-      show_del: false,
-      // upd 那条隐藏
-      show_upd: false,
-
-    },
-  },
-  // 方法
-  methods: {
-    // 关闭弹窗
-    _layer_close: function() {
-      var me = this;
-
-      me.user_obj.title = '';
-      me.user_obj.name = '';
-
-      me.user_obj.show = false;
-    },
-    // 保存数据
-    _layer_save: function() {
-      var me = this;
-
-      // add 
-      if (me.user_obj.show_add) {
-        if (me.user_obj.name == '') {
-          layer.msg('the user`s name should be not empty!');
-          return
-        }
-        var id = me.arr.length + "_" + Math.random();
-        me.arr.push({
-          id: id,
-          name: me.user_obj.name,
-        });
-
-      }
-      // del
-      else if (me.user_obj.show_del) {
-        var ac_index = 0;
-        me.arr.forEach(function(ele, index) {
-          if (ele.id == me.user_obj.ac_id) {
-            ac_index = index;
-          }
-        });
-        me.arr.splice(ac_index, 1);
-
-
-
-      }
-      // upd
-      else if (me.user_obj.show_upd) {
-        if (me.user_obj.name == '') {
-          layer.msg('the user`s name should be not empty!');
-          return
-        }
-        me.arr.forEach(function(ele, index) {
-          if (ele.id == me.user_obj.ac_id) {
-            ele.name = me.user_obj.name;
-          }
-        });
-
-      }
-
-
-
-      // 关闭弹窗的方法
-      me._layer_close();
-    },
-    // ======================================
-    // add
-    _layer_add: function() {
-      var me = this;
-
-      me.user_obj.show = true;
-      // add
-      me.user_obj.show_add = true;
-      // del
-      me.user_obj.show_del = false;
-      // upd
-      me.user_obj.show_upd = false;
-
-      // 标题
-      me.user_obj.title = 'add item';
-      // 初始化名称
-      me.user_obj.name = '';
-    },
-    // 删除
-    _layer_del: function(id) {
-      var me = this;
-      // 记录ID
-      me.user_obj.ac_id = id;
-
-      me.user_obj.show = true;
-
-      // add
-      me.user_obj.show_add = false;
-      // del
-      me.user_obj.show_del = true;
-      // upd
-      me.user_obj.show_upd = false;
-    },
-    _layer_upd: function(id) {
-      var me = this;
-      // 记录ID
-      me.user_obj.ac_id = id;
-      // 大框
-      me.user_obj.show = true;
-
-      // add
-      me.user_obj.show_add = false;
-      // del
-      me.user_obj.show_del = false;
-      // upd
-      me.user_obj.show_upd = true;
-
-      me.user_obj.title = 'upd item';
-      me.arr.forEach(function(ele, index) {
-        if (ele.id == me.user_obj.ac_id) {
-          me.user_obj.name = ele.name;
-        }
-      });
-    },
-
-
-
-
-
-  }
-});
-
-
-var vm = new Vue({
-  // 可以为class dom 
-  el: '#app_2',
-  // 数据
-  data: {
-    info: 'w-vue',
-    // ================================================
-    obj: {
-      a: 'admin',
-      b: 'bdmin',
-      c: 'cdmin',
-      d: 'ddmin',
-    },
-
-
-
-    // ================================================
-    arr: [
-      {
-        name:1,
-        id:1,
-      },
-      {
-        name:2,
-        id:2,
-      },
+    // ------------------------------------------------案例
+    list: [
+      { info: "xxx", id: 1 },
+      { info: "qqq", id: 2 },
+      { info: "eee", id: 3 },
     ],
-    // 用户的对象体
-    user_obj: {
+    // 
+    one: {
       // 整体弹窗的现实
-      show: false,
+      key: false,
 
-      // 信息体
-      title: '',
-      name: '',
-      // 当前点击的ID
-      ac_id: null,
+      // 弹窗显示内容
+      info: "",
 
-      // 添加那条的显示
-      show_add: true,
-      // 删除那条隐藏
-      show_del: false,
-      // upd 那条隐藏
-      show_upd: false,
+      // 标识从哪里发起的弹窗
+      str: "",
 
+      // 修改的数据ID
+      id: "",
     },
   },
   // 方法
   methods: {
+    btn_ck: function() {
+      // console.log(this);
+      this.key = !this.key;
+    },
+
+    // -----------------------------------------------
+    // add
+    _layer_show: function() {
+      // 显示
+      this.one.key = true;
+      // 
+      this.one.info = '';
+
+      // 从新增出点击进入
+      this.one.str = "add";
+    },
     // 关闭弹窗
     _layer_close: function() {
-      var me = this;
-
-      me.user_obj.title = '';
-      me.user_obj.name = '';
-
-      me.user_obj.show = false;
+      // 
+      this.one.key = false;
+      // 
+      this.one.info = '';
     },
     // 保存数据
-    _layer_save: function() {
-      var me = this;
+    _layer_sure: function() {
 
-      // add 
-      if (me.user_obj.show_add) {
-        if (me.user_obj.name == '') {
-          layer.msg('the user`s name should be not empty!');
-          return
-        }
-        var id = me.arr.length + "_" + Math.random();
-        me.arr.push({
+
+      // 新增
+      if (this.one.str == "add") {
+        var id = this.list.length + "_" + Math.random();
+        this.list.unshift({
           id: id,
-          name: me.user_obj.name,
+          info: this.one.info,
         });
-
-      }
-      // del
-      else if (me.user_obj.show_del) {
-        var ac_index = 0;
-        me.arr.forEach(function(ele, index) {
-          if (ele.id == me.user_obj.ac_id) {
-            ac_index = index;
-          }
-        });
-        me.arr.splice(ac_index, 1);
-
-
-
       }
       // upd
-      else if (me.user_obj.show_upd) {
-        if (me.user_obj.name == '') {
-          layer.msg('the user`s name should be not empty!');
-          return
+      else if (this.one.str == "upd") {
+        // 找到信息
+        this.list.some(function(ele) {
+          if (ele.id == this.one.id) {
+            ele.info = this.one.info;
+            return true;
+          }
+        }.bind(this));
+      }
+
+
+      this._layer_close();
+    },
+
+    _layer_upd: function(id) {
+
+      // 显示弹出
+      this.one.key = true;
+
+      this.one.id = id;
+
+      // 找到信息
+      this.list.some(function(ele) {
+        if (ele.id == id) {
+          this.one.info = ele.info;
+          return true;
         }
-        me.arr.forEach(function(ele, index) {
-          if (ele.id == me.user_obj.ac_id) {
-            ele.name = me.user_obj.name;
-          }
-        });
+      }.bind(this));
 
-      }
-
-
-
-      // 关闭弹窗的方法
-      me._layer_close();
+      // 标识
+      this.one.str = "upd";
     },
-    // ======================================
-    // add
-    _layer_add: function() {
-      var me = this;
 
-      me.user_obj.show = true;
-      // add
-      me.user_obj.show_add = true;
-      // del
-      me.user_obj.show_del = false;
-      // upd
-      me.user_obj.show_upd = false;
-
-      // 标题
-      me.user_obj.title = 'add item';
-      // 初始化名称
-      me.user_obj.name = '';
-    },
     // 删除
     _layer_del: function(id) {
-      var me = this;
-      // 记录ID
-      me.user_obj.ac_id = id;
-
-      me.user_obj.show = true;
-
-      // add
-      me.user_obj.show_add = false;
-      // del
-      me.user_obj.show_del = true;
-      // upd
-      me.user_obj.show_upd = false;
-    },
-    _layer_upd: function(id) {
-      var me = this;
-      // 记录ID
-      me.user_obj.ac_id = id;
-      // 大框
-      me.user_obj.show = true;
-
-      // add
-      me.user_obj.show_add = false;
-      // del
-      me.user_obj.show_del = false;
-      // upd
-      me.user_obj.show_upd = true;
-
-      me.user_obj.title = 'upd item';
-      me.arr.forEach(function(ele, index) {
-        if (ele.id == me.user_obj.ac_id) {
-          me.user_obj.name = ele.name;
+      this.list.some(function(ele, index) {
+        if (ele.id == id) {
+          this.list.splice(index, 1);
+          return true;
         }
-      });
+      }.bind(this));
     },
+
 
 
 
--- src_webapp/demo_001/readme.md
+++ src_webapp/demo_001/readme.md
@@ -1,46 +1,282 @@
-# Vue step 1
+# Vue step-01
 
-* 总的来说：vue的思想就是非DOM操，直接面对是数据和DOM树，那么这两者之间靠什么连接？
-* 就是v-指令，和vm数据模型。其实背后就是数据监听和发布订阅者者模式；
-* 所以【DOM树】+【数据】+【vm数据模型】共同构成组件cpt，也就是功能强大的模板引擎。
-* 后面的学习Vue自定义指令的时候，内部其实更多的操作的还是DOM，所以v-指令是什么？是DOM操作。
+## 理解
 
-### 指令
+* 总的来说，vue的思想就是非DOM操，直接面对是数据和DOM树，那么这两者之间靠什么连接？
+  * v-指令
+  * vm数据模型
+  * 背后：数据监听和发布订阅者者模式；
+* 【DOM树】+【数据】+【vm数据模型】共同构成组件cpt，也就是功能强大的模板引擎。
+* 后面的学习Vue自定义指令的时候，内部其实更多的操作的还是DOM。
 
+## 初始化
+
+```js
+var vm = new Vue({
+  // 可以为class dom 
+  el: '#app',
+  // 数据
+  data: {},
+  // 方法
+  methods: {}
+});
 ```
-{{key}} 任何变量都可注入
-<input type="text" value="{{info}}">
-{{*key}}  只绑定一次数据
-{{{}}} 识别HTML结构
 
-v-model = "key" 
+## 语法
 
-v-text 大段text注入
-v-html html注入
+* {{key}} 与 v-model
 
-[v-cloak]{ 在编译完成之前
-  display:none;
-}
+```html
+<div class="box">
+    <h1>v-model</h1>
+    <div class="item">
+        输入：<input type="text" v-model="obj.a">
+    </div>
+    <div class="item">
+        显示：{{obj.a}}
+    </div>
+    <div class="item">
+        一次绑定：{{*obj.a}}
+    </div>
+    <div class="item">
+        识别HTML：{{{obj.a}}}
+    </div>
+
+</div>
+
+<script>
+    // {{key}} 任何属性名都可注入 <input type="text" value="{{info}}">
+    // {{*key}} 只绑定一次数据
+    // {{{}}}   识别HTML结构
+    // {{obj.a=="admin"}}  也可以注入简单的表达式
+
+    // v-model = "key" 指令是直接注入变量
+    // v-text 大段text注入
+    // v-html html注入
+</script>
+
 
  v-show='a';a:true
  v-show='arr.length==0';
 
+
+
+```
+
+* v-for
+
+```html
 <div class="item" v-for = 'ele in list'>   track-by='索引' 提高循环性能
   {{ele}}  自带参数 $index
 </div>
 
-<div class="item" v-for = 'val in obj' >  v-for = '(k,v) in obj'
+<div class="item" v-for = 'val in obj' >   v-for = '(k,v) in obj'
   {{val}} -每项的val  自带参数$index/$key
 </div>
-
-v-on:click="btn_1()"   v-on:click="a=false"
-方法内部的this，是实例，可拿到所有的方法和数据
-
-v-on:click="_layer_del(ele.id)" 可在循环的事件绑定中插入数据
 ```
 
-### 总结
+* v-show  v-on:click
 
-* 1.新增弹窗业务：新出现的DOM树，DOM树内写v-指令是没有用的，只能是DOM树一开始要全部存在。所以对该业务，可以通过数据控制一片DOM的显示和隐藏。
-* 2.因为是DOM+数据，表现为视图的HTML内会有类似JS的代码，其实这就是模板引擎。
-* 3.查看某个包的版本信息：`bower info vue 或者npm info package`
+```html
+<div class="box">
+    <h1>v-show</h1>
+    <div class="item">
+        <button v-on:click="key = !key">btn</button>
+    </div>
+    <div class="item">
+        <button v-on:click="btn_ck()">btn</button>
+    </div>
+
+    <div class="item" v-show=key>
+        显示的信息
+    </div>
+</div>
+
+<script>
+    // v-on:click="btn_ck()";  
+    // 方法内部的this是实例对象，上面可以拿到所有的方法和属性；
+    btn_ck: function() {
+      // console.log(this);
+      this.key = !this.key;
+    },
+    
+    // v-on:click="key = !key" 可以直接进行属性操作；
+</script>
+```
+
+* v-cloak：遮丑
+
+```css
+// v-cloak在vue编译之前存在，存在的时候我们作为自定义属性存在，设置存在时隐藏
+[v-cloak]{ 
+  display:none;
+}
+// 编译完成后，消失；
+```
+
+* 查看某个包的版本信息：`bower info vue 或者npm info package`
+
+## 案例
+
+* 需求：
+
+  * 初始化：列表显示；
+    * 有数据：列表展示；
+    * 没有数据：列表隐藏，且显示没有数据；
+  * 点击新增：弹窗显示，
+    * 确认：弹窗隐藏，内容新增一条，列表显示；
+    * 取消：弹窗隐藏
+
+  * 点击列表修改：弹窗显示
+    * 确认：弹窗隐藏，内容被修改一条，列表显示；
+    * 取消：弹窗隐藏
+  * 点击列表删除：直接删除
+
+* 分析：
+  * 初始化：列表显示；通过数组的长度控制
+  * 点击新增：弹窗显示，标识为从点击add后出现弹窗
+  * 点击列表修改：弹窗显示，标识为从点击upd后出现弹窗，记录id
+  * 点击列表删除：传入id，删除数据；
+
+### 列表初始化
+
+* v-show="list.length!=0"
+
+```html
+<div class="item" v-for="ele in list" v-show="list.length!=0">
+    {{$index+1}} {{ele.info}} <button v-on:click="_layer_upd(ele.id)">upd</button> <button v-on:click="_layer_del(ele.id)">del</button>
+</div>
+<div class="item" v-show="list.length==0">
+    当前列表没有数据
+</div>
+```
+
+### add_sure
+
+```html
+<button v-on:click="_layer_show()">add</button>
+<script>
+    var obj = {
+        data:{
+            // 
+            one: {
+                // 整体弹窗的现实
+                key: false,
+
+                // 弹窗显示内容
+                info: "",
+
+                // 标识从哪里发起的弹窗
+                str: "",
+
+                // 修改的数据ID
+                id: "",
+            },
+        }
+        methods:{
+            // add
+            _layer_show: function() {
+                // 显示
+                this.one.key = true;
+                // 
+                this.one.info = '';
+
+                // 从新增出点击进入
+                this.one.str = "add";
+            },
+            // 保存数据
+            _layer_sure: function() {
+
+                // 新增
+                if (this.one.str == "add") {
+                    var id = this.list.length + "_" + Math.random();
+                    this.list.unshift({
+                        id: id,
+                        info: this.one.info,
+                    });
+                }
+                // upd
+                else if (this.one.str == "upd") {
+                    // 找到信息
+                    ...
+                }
+
+                    this._layer_close();
+                }, 
+            }
+    	}
+    };
+</script>
+```
+
+### upd_sure
+
+```html
+<div class="item" v-for="ele in list" v-show="list.length!=0">
+    {{$index+1}} {{ele.info}} 
+    <button v-on:click="_layer_upd(ele.id)">upd</button> 
+    <button v-on:click="_layer_del(ele.id)">del</button>
+</div>
+<script>
+    var obj = {
+        methods:{
+            _layer_upd: function(id) {
+
+                // 显示弹出
+                this.one.key = true;
+                // 
+                this.one.id = id;
+
+                // 找到信息
+                this.list.some(function(ele) {
+                    if (ele.id == id) {
+                        this.one.info = ele.info;
+                        return true;
+                    }
+                }.bind(this));
+                // 标识
+                this.one.str = "upd";
+            },
+            // 保存数据
+            _layer_sure: function() {
+
+                // 新增
+                if (this.one.str == "add") {
+                    ...
+                }
+                // upd
+                else if (this.one.str == "upd") {
+                    // 找到信息
+                    this.list.some(function(ele) {
+                        if (ele.id == this.one.id) {
+                            ele.info = this.one.info;
+                            return true;
+                        }
+                    }.bind(this));
+                }
+
+                this._layer_close();
+            },
+        }
+    };
+</script>
+```
+
+### del
+
+```js
+// 删除
+_layer_del: function(id) {
+    this.list.some(function(ele, index) {
+        if (ele.id == id) {
+            this.list.splice(index, 1);
+            return true;
+        }
+    }.bind(this));
+},
+```
+
+
+
+
+
